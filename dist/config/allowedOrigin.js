dacf8f7ad81cea2b9bc5c3e38a8d866043b8af90
ivan01-tech <ivansilatsa@gmail.com>
2024-03-06 18:51:48 +0100

    cors

--- dist/config/allowedOrigin.js
+++ dist/config/allowedOrigin.js
@@ -2,4 +2,5 @@ export const allowedOrigns = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://127.0.0.1:5500',
+    'https://mapiol-frontend.onrender.com',
 ];
--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -7,5 +7,5 @@ export const allowedOrigns = [
   'http://localhost:3500',
   'http://localhost:3000',
   'http://127.0.0.1:5500',
-  // process.env.CLIENT_URL,
+  'https://mapiol-frontend.onrender.com',
 ];
